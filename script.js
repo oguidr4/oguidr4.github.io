06f0ebc4f0c58c55d8ad712f9348435544ffa826
oguidr4 <35343404+oguidr4@users.noreply.github.com>
2018-02-08 12:11:28 -0600

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+let projectId = 1
+let projectTitle = "Story Map"
+let thumbnail = true
+let projectTitles = ["Story Map", "Map of Pittsburgh"]
+
+console.log(projectId)
+console.log(projectTitle)
+console.log(thumbnail)
+console.log(projectTitles)
