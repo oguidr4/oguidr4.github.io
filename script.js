06c98cc1167d657644aec0d16a5af9cfe0252dfa
oguidr4 <35343404+oguidr4@users.noreply.github.com>
2018-02-15 11:48:48 -0600

    javascript update

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let projectId = 1
-let projectTitle = "Story Map"
-let thumbnail = true
-let projectTitles = ["Story Map", "Map of Pittsburgh"]
+let ProjectArray = [
+  "1",
+  "Story Map",
+  "https://www.arcgis.com/apps/MapTour/index.html?appid=5fdd5b64b4d849e0a68f3d016d0d91c3"
+  true,
+  ["pittsburgh", "map tour"]
+]
 
-console.log(projectId)
-console.log(projectTitle)
-console.log(thumbnail)
-console.log(projectTitles)
+let ProjectObject = {
+  id: 1,
+  url: "https://www.arcgis.com/apps/MapTour/index.html?appid=5fdd5b64b4d849e0a68f3d016d0d91c3",
+  title: "Book Assignment 1 - Pittsburgh",
+  hasThumbnail: true,
+  keywords: ["pittsburgh", "map tour"]
+}
+
+console.log(ProjectArray)
+console.log(ProjectObject)
