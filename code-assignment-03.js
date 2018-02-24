4489bb483e8c9da219f90a9b86c515767bbd4e8d
oguidr4 <35343404+oguidr4@users.noreply.github.com>
2018-02-24 13:30:54 -0600

    Book Assignment 6a

---
+++ code-assignment-03.js
@@ -0,0 +1,13 @@
+let projectid = 1
+let projecttitle = "Book Assignment 1"
+let hasthumbnail = true
+let projectarray = ["Story Map", "Pittsburgh", "GEOG4046"]
+
+console.log(projectid);
+console.log(projecttitle);
+console.log(hasthumbnail);
+console.log(projectarray);
+console.log("Project ID: " + projectid);
+console.log("Title: " + projecttitle);
+console.log("Does it have a thumbnail image?" + hasthumbnail);
+console.log('Keywords: ' + projectarray);
---
+++ code-assignment-04.js
@@ -0,0 +1,21 @@
+let ProjectArray = [
+  "1",
+  "Story Map",
+  "https://www.arcgis.com/apps/MapTour/index.html?appid=5fdd5b64b4d849e0a68f3d016d0d91c3"
+  true,
+  ["pittsburgh", "map tour"]
+]
+
+let ProjectObject = {
+  id: 1,
+  url: "https://www.arcgis.com/apps/MapTour/index.html?appid=5fdd5b64b4d849e0a68f3d016d0d91c3",
+  title: "Book Assignment 1 - Pittsburgh",
+  hasThumbnail: true,
+  keywords: ["pittsburgh", "map tour"]
+}
+
+console.log(ProjectArray)
+console.log(ProjectObject)
+console.log(ProjectArray[2])
+console.log(ProjectObject.title)
+console.log(ProjectObject["title"])
